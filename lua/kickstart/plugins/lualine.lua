bbbc1b18ccfd3e6fb2ff859244868b722b0bbe89
monsterooo <createthink.net@gmail.com>
2025-01-29 22:09:50 +0800

    更新lualine配置

--- lua/kickstart/plugins/lualine.lua
+++ lua/kickstart/plugins/lualine.lua
@@ -20,12 +20,13 @@ return {
         lualine_z = { 'location' },
       },
       tabline = {
-        lualine_a = { 'tabs' },
-        lualine_b = { 'buffers' },
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {},
+        -- 顶部栏不设置任何东西，因为它会覆盖掉bufferline的tab
+        -- lualine_a = { 'tabs' },
+        -- lualine_b = { 'buffers' },
+        -- lualine_c = {},
+        -- lualine_x = {},
+        -- lualine_y = {},
+        -- lualine_z = {},
       },
     }
   end,
