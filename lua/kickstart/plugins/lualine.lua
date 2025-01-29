aae753851b4a7fb9dba0804c52555017a032e562
monsterooo <createthink.net@gmail.com>
2025-01-29 21:54:08 +0800

    增加lualine状态栏插件

---
+++ lua/kickstart/plugins/lualine.lua
@@ -0,0 +1,32 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  event = "VeryLazy",
+  config = function()
+    require('lualine').setup {
+      options = {
+        theme = 'gruvbox',
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename', 'lsp_progress' },
+        lualine_x = {
+          'encoding',
+          'fileformat',
+          'filetype',
+        },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+      tabline = {
+        lualine_a = { 'tabs' },
+        lualine_b = { 'buffers' },
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      },
+    }
+  end,
+}
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -44,7 +44,7 @@ require('lazy').setup({
 
   require 'kickstart/plugins/todo-comments',
 
-  require 'kickstart/plugins/mini',
+  -- require 'kickstart/plugins/mini',
 
   require 'kickstart/plugins/treesitter',
 
@@ -55,6 +55,8 @@ require('lazy').setup({
   require 'kickstart/plugins/supermaven',
 
   require 'kickstart/plugins/comment',
+
+  require 'kickstart/plugins/lualine',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
