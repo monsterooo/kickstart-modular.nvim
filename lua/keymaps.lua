9482b285f5f58a202a2d2c705871b0819fc5535c
monsterooo <createthink.net@gmail.com>
2025-01-30 23:03:23 +0800

    增加拷贝到系统剪贴板映射

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -51,3 +51,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.keymap.set('n', '<C-s>', ':w<CR>', { desc = 'Save file' })
 vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>', { desc = 'Save file' })
 vim.keymap.set('v', '<C-s>', '<Esc>:w<CR>', { desc = 'Save file' })
+
+-- 拷贝到系统剪贴板
+vim.keymap.set('v', '<leader>y', [["+y]], { desc = 'Yanking into Clipboard' })
